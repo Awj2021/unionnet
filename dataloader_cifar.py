9747fda6e0adba46b112d66fd7e37079059e2695
wenjie <wenjie.ai10@gmail.com>
2024-07-28 09:33:39 +0100

    w/ data transform, e.g., flip & crop & specific normalization.

--- dataloader_cifar.py
+++ dataloader_cifar.py
@@ -6,16 +6,16 @@ import torchvision.datasets as datasets
 from cifar import CIFAR10_MR # just setting the multi rater dataset.
 
 def load_mr_data(batchsize:int, numworkers:int, args):
-    trans = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-        ])
-    
     # trans = transforms.Compose([
-    #         transforms.RandomHorizontalFlip(),
-    #         transforms.RandomCrop(32, padding=4),
     #         transforms.ToTensor(),
-    #         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
+    #         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+    #     ])
+    
+    trans = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, padding=4),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))])
     
     data_train = CIFAR10_MR(
                         root = './',
@@ -38,15 +38,15 @@ def load_mr_data(batchsize:int, numworkers:int, args):
 
 
 def load_clean_test_data(batchsize:int, numworkers:int) -> DataLoader:
-    trans = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
-        ])
-    
     # trans = transforms.Compose([
     #         transforms.ToTensor(),
     #         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
     #     ])
+    
+    trans = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ])
     data_test = CIFAR10(
                         root = './',
                         train = False,
